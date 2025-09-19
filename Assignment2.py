fdef2a12b5b5ba0b64a4ac9d1574d6f5d031a5d8
Masonator24 <mgilbe49@students.kennesaw.edu>
2025-09-19 16:13:43 -0400

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -1,3 +1,10 @@
+	# Program Name: Assignment2.py (use the name the program is saved as)
+	# Course: IT3883/Section W02
+	# Student Name: Mason Gilbert
+	# Assignment Number: Assignment 2
+	# Due Date: 09/19/2025
+	# Purpose: What does the program do (in a few sentences)? This program uses 9 student's grades and calculates the final average for each student. It will print their name and final average in descending order by grade.
+	# List Specific resources used to complete the assignment. (PyCharm as an IDE), GeeksforGeeks, W3Schools
 grade_file = "Assignment2input.txt"
 students = []
 
