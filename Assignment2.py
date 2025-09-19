551f5b961965427e918112ea288a9419b2fa70af
Masonator24 <mgilbe49@students.kennesaw.edu>
2025-09-19 16:19:40 -0400

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -6,16 +6,17 @@
 	# Purpose: What does the program do (in a few sentences)? This program uses 9 student's grades and calculates the final average for each student. It will print their name and final average in descending order by grade.
 	# List Specific resources used to complete the assignment. (PyCharm as an IDE), GeeksforGeeks, W3Schools, Python.org
 grade_file = "Assignment2input.txt"
-students = []
+students = [] #List for students
 
-with open(grade_file, 'r') as files:
+with open(grade_file, 'r') as files: #This will allow the file in read mode
     for line in files:
         sections = line.strip().split()
         if len(sections) == 7:
+			#Variables
             name = sections[0]
             score = list(map(float, sections[1:]))
             average = sum(score)/ len(score)
             students.append((name,average))
-students.sort(key= lambda x: x[1], reverse=True)
+students.sort(key= lambda x: x[1], reverse=True) #This sorts by average in descending order
 for name, average in students:
-    print(f"{name} {average: .2f}")
+    print(f"{name} {average: .2f}") #This prints the results
