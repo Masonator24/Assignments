f62541bace93abc3022ab7b8c9e6aa6de555df51
Masonator24 <mgilbe49@students.kennesaw.edu>
2025-10-26 16:58:47 -0400

    Update Assignment4A.py

--- Assignment4A.py
+++ Assignment4A.py
@@ -5,6 +5,7 @@
 	# Due Date: 10/26/2025
 	# Purpose: This program allows for users to send a string to a connected server. It will then be sent and converted into upper case and shall be sent back to the user.
   # List Specific resources used to complete the assignment. (PyCharm as an IDE), Stackflow, Baeldung, Python.org, KSU Slideshows
+import socket
 server_ip = "127.0.0.1"
 port = 40021
 socket_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
